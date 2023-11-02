243a55fc091d1f136884975cc5ae836c97277d5a
piet <pprotassow@posteo.de>
2023-11-02 13:21:49 +0100

    browsable api

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Todo
 
-# Create your tests here.
+
+class TodoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.todo = Todo.objects.create(
+            title="first todo",
+            body="a body of text"
+        )
+
+    def test_model_content(self):
+        self.assertEqual(self.todo.title, "first todo")
+        self.assertEqual(str(self.todo), "first todo")
+        self.assertEqual(self.todo.body, "a body of text")
